0127bc045a2b1fde6497621bbdffe023f95c2098
giuseppeesposito22 <gius.espo22@gmail.com>
2025-07-30 21:31:16 +0200

    add subclass Cuffie

---
+++ src/org/lessons/java/shop/Cuffie.java
@@ -0,0 +1,39 @@
+package org.lessons.java.shop;
+
+public class Cuffie extends Prodotto{
+    
+    private String colore;
+    private boolean isWireless;
+
+    
+    public Cuffie(String nome, String descrizione, float prezzo, int iva, String colore, boolean isWireless) {
+        super(nome, descrizione, prezzo, iva);
+        this.colore = colore;
+        this.isWireless = isWireless;
+    }
+
+
+    public String getColore() {
+        return colore;
+    }
+
+
+    public void setColore(String colore) {
+        this.colore = colore;
+    }
+
+
+    public boolean isWireless() {
+        return isWireless;
+    }
+
+
+    public void setWireless(boolean isWireless) {
+        this.isWireless = isWireless;
+    }
+
+
+
+
+
+}
